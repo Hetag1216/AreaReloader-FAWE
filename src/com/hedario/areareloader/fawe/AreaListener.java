bb09eb74e78cb39b023664607eb97a832f67e339
Hetag1216 <dariob450@gmail.com>
2023-03-02 12:44:31 +0100

    Version 1.1
    Fixed issue where /ar help command would not show the command list correctly.
    Split package merging with AreaReloader > AreaReloader-FAWE

---
+++ src/com/hedario/areareloader/fawe/AreaListener.java
@@ -0,0 +1,11 @@
+package com.hedario.areareloader.fawe;
+
+import org.bukkit.event.Listener;
+
+public class AreaListener implements Listener {
+	AreaReloader plugin;
+
+	public AreaListener(AreaReloader plugin) {
+		this.plugin = plugin;
+	}
+}
