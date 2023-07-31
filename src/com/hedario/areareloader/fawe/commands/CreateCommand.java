555c177fbf138da02a2a3be85c20554817a7adb9
Hetag1216 <dariob450@gmail.com>
2023-07-31 16:48:04 +0200

    Added async area creation

--- src/com/hedario/areareloader/fawe/commands/CreateCommand.java
+++ src/com/hedario/areareloader/fawe/commands/CreateCommand.java
@@ -3,9 +3,9 @@ package com.hedario.areareloader.fawe.commands;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import com.hedario.areareloader.fawe.AreaMethods;
 import com.hedario.areareloader.fawe.AreaReloader;
@@ -14,8 +14,9 @@ import com.sk89q.worldedit.WorldEditException;
 
 public class CreateCommand extends ARCommand {
 	private boolean skipE, skipB;
+
 	public CreateCommand() {
-		super("create", "/ar create <name> <copyEntities: true|false> <copyBiomes: true|false>", formatColors(Manager.getConfig().getString("Commands.Create.Description")), new String[] { "create" });
+		super("create", "/ar create <name> <copyEntities: true|false> <copyBiomes: true|false>", Manager.getConfig().getString("Commands.Create.Description"), new String[] { "create" });
 	}
 
 	@Override
@@ -23,54 +24,66 @@ public class CreateCommand extends ARCommand {
 		if (!hasPermission(sender) || !isPlayer(sender) || !this.correctLength(sender, args.size(), 3, 3)) {
 			return;
 		}
-			String area = args.get(0);
-			//boolean skipEntities = false;
-			//boolean copyBiomes = false;
-			if (area.equalsIgnoreCase("all")) {
-				sendMessage(sender, invalidName().replaceAll("%area%", area), true);
-				return;
-			}
-			if (Manager.areas.getConfig().contains("Areas." + args.get(0))) {
-				sendMessage(sender, exists().replaceAll("%area%", area), true);
-				return;
-			}
-			final String skipEnts = args.get(1);
-			if (skipEnts.equalsIgnoreCase("true")) {
-				skipE= true;
-				//skipEntities = true;
-			} else if (skipEnts.equalsIgnoreCase("false")) {
-				//skipEntities = false;
-				skipE= false;
-			} else {
-				sendMessage(sender, invalidValue(), true);
-				return;
-			}
-			final String biomes = args.get(2);
-			if (biomes.equalsIgnoreCase("true")) {
-				//copyBiomes = true;
-				skipB = true;
-			} else {
-				skipB = false;
-				//copyBiomes = false;
-			}
-			try {
-				sendMessage(sender, preparing().replaceAll("%area%", area), true);
-				Bukkit.getScheduler().runTaskAsynchronously(AreaReloader.plugin, () -> {
+		String area = args.get(0);
+		if (area.equalsIgnoreCase("all")) {
+			sendMessage(sender, invalidName().replaceAll("%area%", area), true);
+			return;
+		}
+		if (Manager.areas.getConfig().contains("Areas." + args.get(0))) {
+			sendMessage(sender, exists().replaceAll("%area%", area), true);
+			return;
+		}
+		final String skipEnts = args.get(1);
+		if (skipEnts.equalsIgnoreCase("true")) {
+			skipE = true;
+		} else if (skipEnts.equalsIgnoreCase("false")) {
+			skipE = false;
+		} else {
+			sendMessage(sender, invalidValue(), true);
+			return;
+		}
+		final String biomes = args.get(2);
+		if (biomes.equalsIgnoreCase("true")) {
+			skipB = true;
+		} else {
+			skipB = false;
+		}
+		try {
+			BukkitRunnable br = new BukkitRunnable() {
+				@Override
+				public void run() {
+					sendMessage(sender, preparing().replaceAll("%area%", area), true);
 					if (AreaMethods.createNewArea((Player) sender, args.get(0), 16, skipE, skipB)) {
 						sendMessage(sender, success().replaceAll("%area%", area), true);
+						if (AreaMethods.isAsyncCreation && AreaMethods.creations.contains(area)) {
+							AreaMethods.creations.remove(area);
+						}
 					} else {
 						sendMessage(sender, fail().replaceAll("%area%", area), true);
 					}
-				});
-			} catch (WorldEditException e) {
-				Manager.printDebug(this.getName(), e, sender);
+				}
+			};
+			
+			if (isAsync()) {
+				br.runTaskAsynchronously(AreaReloader.getInstance());
+				AreaMethods.creations.add(area);
+			} else {
+				br.runTask(AreaReloader.getInstance());
 			}
+
+		} catch (WorldEditException e) {
+			Manager.printDebug(this.getName(), e, sender);
+		}
+	}
+	
+	private boolean isAsync() {
+		return Manager.getConfig().getBoolean("Commands.Create.Asynchronously");
 	}
 
 	private String preparing() {
 		return Manager.getConfig().getString("Commands.Create.Preparing");
 	}
-	
+
 	private String exists() {
 		return Manager.getConfig().getString("Commands.Create.AlreadyExists");
 	}
@@ -82,15 +95,15 @@ public class CreateCommand extends ARCommand {
 	private String fail() {
 		return Manager.getConfig().getString("Commands.Create.Failure");
 	}
-	
+
 	private String invalidName() {
 		return Manager.getConfig().getString("Commands.Create.InvalidName");
 	}
-	
+
 	private String invalidValue() {
 		return Manager.getConfig().getString("Commands.Create.InvalidValue");
 	}
-	
+
 	@Override
 	protected List<String> getTabCompletion(final CommandSender sender, final List<String> args) {
 		List<String> list = new ArrayList<String>();
