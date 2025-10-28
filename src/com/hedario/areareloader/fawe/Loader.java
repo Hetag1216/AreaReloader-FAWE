58a32539dfcf357eecfd8e8da792699676ca281b
Hetag1216 <dariob450@gmail.com>
2025-10-28 23:10:23 +0100

    Finalised new area loader
    - Improved section loading accuracy.
    - Improved task cancel handling.

--- src/com/hedario/areareloader/fawe/Loader.java
+++ src/com/hedario/areareloader/fawe/Loader.java
@@ -14,6 +14,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
+import com.hedario.areareloader.fawe.AreaMethods.Phase;
 import com.hedario.areareloader.fawe.configuration.Manager;
 import com.hedario.areareloader.fawe.events.AreaCompleteEvent;
 import com.hedario.areareloader.fawe.events.AreaLoadEvent;
@@ -28,24 +29,19 @@ public class Loader {
 	private static final Map<String, Loader> INSTANCES = new HashMap<String, Loader>();
 	private final String name;
 	private final Location location;
-	@SuppressWarnings("unused")
-	private final int maxX, maxZ, length, maxChunks;
-	private int x, z, chunks;
+	private final int maxX, maxZ, length;
+	private int x, z, percentage;
+	public int perc, lastperc;
 	private CommandSender sender;
-	private long startTime, tick;
+	private long startTime;
 	private boolean completed;
-	private BukkitTask task;
-	private int percentage;
+	public BukkitTask task;
 	
 	public Loader(String area, Location location, int x, int z, CommandSender sender) {
-		AreaMethods.sendMessage(sender, "called", true);
 		this.name = area;
 		this.location = location;
 		this.maxX = x;
 		this.maxZ = z;
-		x++;
-		z++;
-		this.maxChunks = AreaMethods.getAreaChunk(area);
 		this.length = (AreaMethods.getAreaLength(area) != null && AreaMethods.getAreaLength(area) > 0) ? AreaMethods.getAreaLength(area) : 16;
 		this.x = 0;
 		this.z = 0;
@@ -63,44 +59,40 @@ public class Loader {
 			Bukkit.getServer().getPluginManager().callEvent(new AreaLoadEvent(area));
 		}
 		this.startTime = System.currentTimeMillis();
-		this.tick = startTime;
-		AreaMethods.sendMessage(sender, "pre put", true);
 		this.percentage = Manager.getConfig().getInt("Settings.AreaLoading.Percentage");
-		AreaMethods.sendMessage(sender, "put", true);
 		INSTANCES.put(area, this);
 		start();
-		AreaMethods.sendMessage(sender, "started", true);
 	}
 	
 	private void progress() throws FileNotFoundException, WorldEditException, IOException {
-		if (System.currentTimeMillis() >= tick + AreaMethods.getInterval(this.name) && !completed) {
+		if (!completed) {
 			if (!AreaMethods.loadSchematicArea(sender, this.name, AreaMethods.getFileName(this.name, x, z), location.getWorld(), location.clone().add(x * length, 0.0D, z * length))) {
 				if (sender instanceof Player) {
 					((Player) sender).getWorld().playSound(((Player) sender).getLocation(), Sound.ENTITY_VILLAGER_NO, 1F, 0.5F);
-					AreaMethods.sendMessage(sender, "couldn't load", true);
 				}
 				return;
 			} else {
-				chunks += 1;
-				z += 1;
-				tick = System.currentTimeMillis();
+				if (x == this.maxX && z == this.maxZ) {
+					this.completed = true;
+					return;
+				}
+				z++;
 				if (z > this.maxZ) {
 					z = 0;
-					x += 1;
+					x++;
 				}
-				int perc = (chunks * 100) / maxChunks;
-				if (perc % percentage == 0) {
+				if (x > this.maxX) {
+					x = this.maxX;
+				}
+				int total = (this.maxX + 1) * (this.maxZ + 1);
+				int processed = x * (this.maxZ + 1) + z;
+				perc = (int) ((processed * 100L) / total);
+				if (perc % percentage == 0 && lastperc != perc) {
 					AreaMethods.sendMessage(sender, process().replace("%area%", name).replace("%perc%", String.valueOf(perc)), true);
 				}
-				AreaMethods.sendMessage(sender, "progressed", true);
+				lastperc = perc;
 			}
-			if (chunks == this.maxChunks) {
-				z -= 1;
-				this.completed = true;
-				AreaMethods.sendMessage(sender, "completed", true);
-			}
-		}
-		if (completed) {
+		} else {
 			if (sender != null) {
 				final long time = System.currentTimeMillis() - startTime;
 				AreaMethods.sendMessage(sender, success().replace("%area%", name).replace("%time%", AreaMethods.formatTime(time)), true);
@@ -111,9 +103,7 @@ public class Loader {
 			} else {
 				Bukkit.getServer().getPluginManager().callEvent(new AreaCompleteEvent(name));
 			}
-			task.cancel();
-			INSTANCES.remove(name);
-			AreaMethods.sendMessage(sender, "finished" + INSTANCES.size(), true);
+			remove();
 		}
 	}
 	
@@ -124,30 +114,43 @@ public class Loader {
 				try {
 					progress();
 				} catch (FileNotFoundException e) {
-					handleError(e, "An error happened while loading the area; file couldn't be found?");
+					handleError(Phase.LOADING, e, "An error happened while loading the area; file couldn't be found?");
 				} catch (WorldEditException | IOException e) {
-					handleError(e, "An error happened while loading the area.");
+					handleError(Phase.LOADING, e, "An error happened while loading the area.");
 				} catch (Throwable e) {
-					handleError(e, "An unexpected error happened while loading the area.");
+					handleError(Phase.LOADING, e, "An unexpected error happened while loading the area.");
 				}
 			}
-		}.runTaskTimer(AreaReloader.plugin, 0, 0);
+		}.runTaskTimer(AreaReloader.plugin, 0, (AreaMethods.getInterval(name) * 20) / 1000);
 	}
 	
 	public static Loader get(final String area) {
 		return INSTANCES.get(area);
 	}
 	
-	private void handleError(Throwable exception, String message) {
-	    AreaMethods.sendMessage(sender, AreaMethods.getPrimaryColor() + message, true);
-	    AreaMethods.sendMessage(sender, AreaMethods.getPrimaryColor() + "Logged to console and debug.", true);
+	public static Map<String, Loader> getInstances() {
+		return INSTANCES;
+	}
 
-	    Manager.printDebug("- AREA LOADING");
+	public void remove() {
+		if (task != null) {
+			if (!task.isCancelled()) {
+				task.cancel();
+			}
+		}
+		INSTANCES.remove(name);
+	}
+	
+	private void handleError(final Phase phase, Throwable exception, String message) {
+		if (sender != null) {
+			AreaMethods.sendMessage(sender, AreaMethods.getPrimaryColor() + message, true);
+			AreaMethods.sendMessage(sender, AreaMethods.getPrimaryColor() + "Logged to console and debug.", true);
+		}
+	    Manager.printDebug("- PHASE: " + phase.name());
 	    Manager.printDebug("Area: " + name);
-	    Manager.printDebug("X, Z: " + x + " " + z);
+	    Manager.printDebug("Section " + x + "_" + z);
 	    Manager.printDebug("File:" + AreaMethods.getFileName(this.name, x, z));
 	    Manager.printDebug("Task ID:" + task.getTaskId());
-	    Manager.printDebug("Chunks: " + chunks + "/" + maxChunks);
 	    Manager.printDebug("Section size: " + length);
 	    Manager.printDebug("Exception type: " + exception.getClass().getSimpleName());
 	    Manager.printDebug("Stack trace: " + exception.getMessage());
