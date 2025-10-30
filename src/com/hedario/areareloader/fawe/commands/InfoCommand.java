58774c5b4332809783d7110fd2cac667a14689f5
Hetag1216 <dariob450@gmail.com>
2025-10-30 12:53:17 +0100

    Update InfoCommand.java
    - Added possibility to select the area's bounds as WE selections (pos1&pos2).
    - Improved text formatting.
    - Fixed issue where loaded % wouldn't show correctly and truncate info.

--- src/com/hedario/areareloader/fawe/commands/InfoCommand.java
+++ src/com/hedario/areareloader/fawe/commands/InfoCommand.java
@@ -12,11 +12,17 @@ import com.hedario.areareloader.fawe.AreaScheduler;
 import com.hedario.areareloader.fawe.Loader;
 import com.hedario.areareloader.fawe.configuration.Manager;
 
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+
 public class InfoCommand extends ARCommand {
 	public InfoCommand() {
 		super("info", "/ar info <area>", Manager.getConfig().getString("Commands.Info.Description"), new String[] { "info" });
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void execute(CommandSender sender, List<String> args) {
 		if (!hasPermission(sender) || !correctLength(sender, args.size(), 1, 1)) {
@@ -34,10 +40,17 @@ public class InfoCommand extends ARCommand {
 			display = "false";
 		}
 		
+		
 		sendMessage(sender, this.getNeutral() + "-=-=-=-=-=-=-=-=-=-=- « " + this.getPrimary() + area + this.getNeutral() + " » -=-=-=-=-=-=-=-=-=-=-", false);
 		sendMessage(sender, this.getPrimary() + "World " + this.getNeutral() + "» " + this.getSecondary() + AreaMethods.getAreaInWorld(area), false);
-		sendMessage(sender, this.getPrimary() + "First corner " + this.getNeutral() + "» " + this.getSecondary() + AreaMethods.getAreaX(area) + this.getNeutral() + ", " + this.getSecondary() + AreaMethods.getAreaY(area)+ this.getNeutral() + ", " + this.getSecondary() + AreaMethods.getAreaZ(area), false);
-		sendMessage(sender, this.getPrimary() + "Second corner " + this.getNeutral() + "» " + this.getSecondary() + AreaMethods.getAreaMaxX(area) + this.getNeutral() + ", " + this.getSecondary() + AreaMethods.getAreaMaxY(area) + this.getNeutral() + ", " + this.getSecondary() + AreaMethods.getAreaMaxZ(area), false);
+		TextComponent pos = new TextComponent(this.getPrimary() + "First corner " + this.getNeutral() + "» " + this.getSecondary() + AreaMethods.getAreaX(area) + this.getNeutral() + ", " + this.getSecondary() + AreaMethods.getAreaY(area)+ this.getNeutral() + ", " + this.getSecondary() + AreaMethods.getAreaZ(area));
+		pos.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(this.getNeutral() + "Click to select pos1").create()));
+		pos.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "//pos1 " + AreaMethods.getAreaX(area) + "," + AreaMethods.getAreaY(area) + "," + AreaMethods.getAreaZ(area)));
+		sender.spigot().sendMessage(pos);
+		pos = new TextComponent(this.getPrimary() + "Second corner " + this.getNeutral() + "» " + this.getSecondary() + AreaMethods.getAreaX(area) + this.getNeutral() + ", " + this.getSecondary() + AreaMethods.getAreaY(area)+ this.getNeutral() + ", " + this.getSecondary() + AreaMethods.getAreaZ(area));
+		pos.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(this.getNeutral() + "Click to select pos2").create()));
+		pos.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "//pos2 " + AreaMethods.getAreaMaxX(area) + "," + AreaMethods.getAreaMaxY(area) + "," + AreaMethods.getAreaMaxZ(area)));
+		sender.spigot().sendMessage(pos);
 		sendMessage(sender, this.getPrimary() + "Chunk size " + this.getNeutral() + "» " + this.getSecondary() + AreaMethods.getAreaChunk(area), false);
 		sendMessage(sender, this.getPrimary() + "Block length " + this.getNeutral() + "» " + this.getSecondary() + ((AreaMethods.getAreaLength(area) != null && AreaMethods.getAreaLength(area) > 0) ? AreaMethods.getAreaLength(area) : 16), false);
 		sendMessage(sender, this.getPrimary() + "Loading Interval " + this.getNeutral() + "» " + this.getSecondary() + (AreaMethods.isGlobalInterval(area) ? AreaMethods.formatTime(AreaMethods.getInterval(area)) + " " + this.getNeutral() + "(" + this.getSecondary() + "GLOBAL" + this.getNeutral() + ")" : AreaMethods.formatTime(AreaMethods.getInterval(area))), false);
@@ -55,9 +68,9 @@ public class InfoCommand extends ARCommand {
 		sendMessage(sender, this.getPrimary() + "Has copied entities " + this.getNeutral() + "» " + this.getSecondary() + Manager.getAreasConfig().getBoolean("Areas." + area + ".HasCopiedEntities"), false);
 		sendMessage(sender, this.getPrimary() + "Has copied biomes " + this.getNeutral() + "» " + this.getSecondary() + Manager.getAreasConfig().getBoolean("Areas." + area + ".HasCopiedBiomes"), false);
 
-			if (Loader.getInstances().containsKey(area)) {
-				sendMessage(sender,this.getPrimary() + "Currently loaded percentage " + this.getNeutral() + "» " + this.getSecondary() + String.format("%.2f", Loader.get(area).perc) + this.getPrimary() + "%", false);
-			}
+		if (Loader.getInstances().containsKey(area)) {
+			sendMessage(sender, this.getPrimary() + "Currently loaded percentage " + this.getNeutral() + "» " + this.getSecondary() + Loader.get(area).perc + "%", false);
+		}
 		sendMessage(sender, this.getPrimary() + "Is automatically reloading " + this.getNeutral() + "» " + this.getSecondary() + Manager.getAreasConfig().getBoolean("Areas." + area + ".AutoReload.Enabled"), false);
 		if (Manager.getAreasConfig().getBoolean("Areas." + area + ".AutoReload.Enabled") == true) {
 			sendMessage(sender, this.getPrimary() + "Auto reloading time " + this.getNeutral() + "» " + this.getSecondary() + AreaMethods.formatTime(Manager.getAreasConfig().getLong("Areas." + area + ".AutoReload.Time")), false);
